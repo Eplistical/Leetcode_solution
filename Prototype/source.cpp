e6c7bb15c0de0abc1a494b40b4354411a34e962f
Eplistical <gaohan@sas.upenn.edu>
2017-06-27 15:30:00 -0400

    add ProtoType

---
+++ Prototype/source.cpp
@@ -0,0 +1,24 @@
+/* 
+ *
+ */
+
+#include "showvec.hpp"
+
+// --- include --- // 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+// --- solution part --- //
+class Solution {
+	public:
+};
+
+
+// --- test part ---//
+int main(int argc, char** argv) {
+	Solution S;
+	return 0;
+}
