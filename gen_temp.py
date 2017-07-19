d40969432f14565b3b8b3cc3fb3bccea802e661b
Eplistical <gaohan@sas.upenn.edu>
2017-07-19 11:33:38 -0400

    modified gen_temp.py

--- gen_temp.py
+++ gen_temp.py
@@ -6,3 +6,4 @@ import os
 title = "{:03d}_{}".format(int(sys.argv[1][:-1]), "".join(sys.argv[2:]))
 os.system('cp -r Prototype ' + title)
 os.system('mv ' + title + '/source.cpp ' + title + '/' + title + '.cpp')
+os.system('mkdir ' + title + '/build');
