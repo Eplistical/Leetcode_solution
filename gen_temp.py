34f29a8ffecc95e4037fb7e3849b7a095aea0960
Eplistical <gaohan@sas.upenn.edu>
2017-07-19 11:17:39 -0400

    added a python script to generate template automatically

---
+++ gen_temp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+title = "{:03d}_{}".format(int(sys.argv[1][:-1]), "".join(sys.argv[2:]))
+os.system('cp -r Prototype ' + title)
+os.system('mv ' + title + '/source.cpp ' + title + '/' + title + '.cpp')
